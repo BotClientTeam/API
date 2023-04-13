327768a39b1b0e14f1d00d7ed11c07d3de262dfc
Taka005 <01taka005@gmail.com>
2023-04-13 21:40:11 +0900

    インデント調整

--- modules/lib/Rest.js
+++ modules/lib/Rest.js
@@ -2,37 +2,37 @@ const fetch = require("node-fetch");
 const BaseURL = "https://discord.com/api/v10"
 
 module.exports = {
-    get: async(token,url)=>{
-      if(!token||!url) throw new Error("No url or token in argument");
+  get: async(token,url)=>{
+    if(!token||!url) throw new Error("No url or token in argument");
 
-      return await fetch(BaseURL+url,{
-        "method": "GET",
-        "headers": {
-          "Content-type": "application/json",
-          "user-agent": "DiscordBot (https://node.js.org, v10)",
-          "Authorization": `Bot ${token}`
-        }
-      })
-      .then(res=>res.json())
-      .catch(error=>{
-          throw error;
-      })
-    },
-    post: async(token,url,body)=>{
-      if(!token||!url||!body) throw new Error("No url, token or body in argument");
-
-      return await fetch(BaseURL+url,{
-        "method": "POST",
-        "headers": {
-          "Content-type": "application/json",
-          "user-agent": "DiscordBot (https://node.js.org, v10)",
-          "Authorization": `Bot ${token}`
-        },
-        "body": JSON.stringify(body)
-      })
-      .then(res=>res.json())
-      .catch(error=>{
+    return await fetch(BaseURL+url,{
+      "method": "GET",
+      "headers": {
+        "Content-type": "application/json",
+        "user-agent": "DiscordBot (https://node.js.org, v10)",
+        "Authorization": `Bot ${token}`
+      }
+    })
+    .then(res=>res.json())
+    .catch(error=>{
         throw error;
-      })
-    }
+    })
+  },
+  post: async(token,url,body)=>{
+    if(!token||!url||!body) throw new Error("No url, token or body in argument");
+
+    return await fetch(BaseURL+url,{
+      "method": "POST",
+      "headers": {
+        "Content-type": "application/json",
+        "user-agent": "DiscordBot (https://node.js.org, v10)",
+        "Authorization": `Bot ${token}`
+      },
+      "body": JSON.stringify(body)
+    })
+    .then(res=>res.json())
+    .catch(error=>{
+      throw error;
+    })
+  }
 }
